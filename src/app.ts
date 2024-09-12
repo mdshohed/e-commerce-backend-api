8382e80331a17f234051116a46e494c632c23982
mdshohed <mdshohed170@gmail.com>
2024-09-12 15:55:12 +0600

    cros orgin updaed

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 // application routes
 app.use("/api/products", ProductRoutes);
 app.use("/api/orders", OrderRoutes);
-app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "*"] }));
+app.use(cors({ origin: "*" }));
 
 
 app.get("/", (req: Request, res: Response) => {
